af37fc3f98e60d6813f352801f56a18204cb5e1f
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-04-16 09:18:33 +0200

    feat(auth-frontend): add icons to input fields
    Refs: #0

--- auth-frontend/src/app/login/login.component.ts
+++ auth-frontend/src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {MatButton} from '@angular/material/button';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
+    MatButton,
+    FaIconComponent,
+    FormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  protected readonly faEnvelope = faEnvelope;
+  protected password: string = '';
 }
