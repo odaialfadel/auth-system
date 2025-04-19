1a57940ae4950defe398baaecbc2ce47133b4b71
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-04-19 11:41:12 +0200

    feat(auth-frontend): optimizations
    Refs: #0

--- auth-frontend/src/app/login/login.component.ts
+++ auth-frontend/src/app/login/login.component.ts
@@ -29,8 +29,8 @@ export class LoginComponent {
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      email: ['', [Validators.email]],
+      password: [''],
     });
   }
 
