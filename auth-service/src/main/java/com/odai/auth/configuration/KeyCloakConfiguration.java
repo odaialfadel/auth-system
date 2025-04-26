58d541086234ded3602f8581cb6bbd842549f291
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-04-26 21:31:12 +0200

    feat(auth-service): create keycloak config
    Refs: #3

---
+++ auth-service/src/main/java/com/odai/auth/configuration/KeyCloakConfiguration.java
@@ -0,0 +1,27 @@
+package com.odai.auth.configuration;
+
+import com.odai.auth.configuration.properties.KeycloakProperties;
+import lombok.AllArgsConstructor;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@AllArgsConstructor
+@Configuration
+public class KeyCloakConfiguration {
+
+    private KeycloakProperties keycloakProperties;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(keycloakProperties.getServerUrl())
+                .realm(keycloakProperties.getRealm())
+                .clientId(keycloakProperties.getClientId())
+                .clientSecret(keycloakProperties.getClientSecret())
+                .username("admin")
+                .password("admin")
+                .build();
+    }
+}
---
+++ auth-service/src/main/java/com/odai/auth/configuration/properties/KeycloakProperties.java
@@ -0,0 +1,31 @@
+package com.odai.auth.configuration.properties;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "keycloak")
+public class KeycloakProperties {
+    @NotBlank
+    private String serverUrl;
+
+    @NotBlank
+    private String realm;
+
+    @NotBlank
+    private String clientId;
+
+    @NotBlank
+    private String clientSecret;
+
+    @NotBlank
+    private String adminUsername;
+
+    @NotBlank
+    private String adminPassword;
+}
