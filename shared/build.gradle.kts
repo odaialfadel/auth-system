09a18384b9404e55b728942ee58996403525f113
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-04-26 15:49:21 +0200

    feat(shared): create shared module - create UserRegistrationRequest
    Refs: #4

---
+++ shared/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "com.odai.shared"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get().toInt()))
+    }
+}
+
+dependencies {
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+    implementation(libs.jakarta.validation.api)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
