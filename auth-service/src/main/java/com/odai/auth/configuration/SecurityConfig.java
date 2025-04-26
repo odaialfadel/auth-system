43db71eed481941d8199932206f2163331414287
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-04-26 21:31:52 +0200

    feat(auth-service): create oauth2 security config
    Refs: #3

---
+++ auth-service/src/main/java/com/odai/auth/configuration/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.odai.auth.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "authentication.active", havingValue = "true", matchIfMissing = true)
+    public SecurityFilterChain authenticatedFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        request -> request
+                                .requestMatchers("/api/**")
+                                .authenticated()
+                                .anyRequest()
+                                .permitAll())
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2
+                                .jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "authentication.active", havingValue = "false")
+    public SecurityFilterChain unauthenticatedFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                request -> request
+                        .anyRequest()
+                        .permitAll());
+        return http.build();
+    }
+}
