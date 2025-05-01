830dba591c6a94cf07ce2184c8f5c01e3b8c8a27
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-05-01 09:38:34 +0200

    feat(auth-service): disable crf if authentication is disabled for Development
    Refs: #3

--- auth-service/src/main/java/com/odai/auth/configuration/SecurityConfig.java
+++ auth-service/src/main/java/com/odai/auth/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,7 +31,8 @@ public class SecurityConfig {
     @Bean
     @ConditionalOnProperty(name = "authentication.active", havingValue = "false")
     public SecurityFilterChain unauthenticatedFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
                 request -> request
                         .anyRequest()
                         .permitAll());
