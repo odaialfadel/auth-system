8e660a20438b550dc1a2e1545494430d43b266d0
Odai Al Fadel <odai.alfadel96@gmail.com>
2025-07-12 14:38:55 +0200

    feat(auth-service): log info token expiration if token obtained when login
    Refs: #21

--- auth-service/src/main/java/com/odai/auth/controller/auth/LoginController.java
+++ auth-service/src/main/java/com/odai/auth/controller/auth/LoginController.java
@@ -4,6 +4,7 @@ import com.odai.auth.gateway.keycloak.KeycloakAuthGateway;
 import com.odai.auth.shared.dto.login.LoginRequest;
 import com.odai.auth.shared.dto.login.LoginResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
 @AllArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 public class LoginController {
@@ -25,6 +27,7 @@ public class LoginController {
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         try {
             AccessTokenResponse token = keycloakAuthGateway.getAccessToken(request.emailOrUsername(), request.password());
+            log.info("Access Token obtained for: {} seconds", token.getExpiresIn());
             return ResponseEntity.ok(new LoginResponse(
                     token.getToken(),
                     token.getExpiresIn(),
